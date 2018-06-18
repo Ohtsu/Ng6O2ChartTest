cf2cb89140c61912df830a0d1d9895aa0383588b
Ohtsu <ohtsu@digipub-net.com>
2018-06-19 03:53:29 +0900

    mod version

--- projects/ng6-o2-chart/src/public_api.ts
+++ projects/ng6-o2-chart/src/public_api.ts
@@ -1,6 +1,6 @@
 /*
  * Public API Surface of ng6-o2-chart
  */
-
+export * from './lib/shared/chart-const';
 export * from './lib/ng6-o2-chart.component';
 export * from './lib/ng6-o2-chart.module';
